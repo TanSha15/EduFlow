f95a9abe92e12ccb13b0fc79045cb5f24f2a3148
TanSha15 <tanishsharma887@gmail.com>
2026-02-21 00:49:56 +0530

    update api url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://eduflow-backend-ygz0.onrender.com",
+    baseURL: "https://eduflow-backend-ygz0.onrender.com/api",
     withCredentials: true, 
 });
 
